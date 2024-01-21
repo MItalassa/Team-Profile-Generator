0f96bf65ca3b3a4d96ce993c2228fee9ae95b920
salvatore vitiello <marina.iacomino@gmail.com>
2024-01-21 17:15:33 +0000

    ManagerInfo() created in index .js to ask in CLI for manager details

--- index.js
+++ index.js
@@ -12,4 +12,55 @@ const render = require("./src/page-template.js");
 
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
+const outputTeam = [];
+// first ask for manager details
+function managerInfo() {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: "What's the manager name?",
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: "What's the manager's employee ID?",
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: "What's the manager email address?",
+            },
+            {
+                type: 'input',
+                name: 'officeNumber',
+                message: "What's the manager's office number'?",
+            },
+])
+.then((val) => {
+    const manager = new Manager(val.name, val.id, val.email, val.officeNumber)
+    outputTeam.push(manager)
+    completeTeam()
+})
+};
+function completeTeam() {
+    inquirer
+        .prompt([
+            {
+    type: 'confirm',
+    name: 'askAgain',
+    message: 'Want to enter another TV show favorite (just hit enter for YES)?',
+    default: true,
+},
+function ask() {
+  inquirer.prompt(questions).then((answers) => {
+    output.push(answers.tvShow);
+    if (answers.askAgain) {
+      ask();
+    } else {
+      console.log('Your favorite TV Shows:', output.join(', '));
+    }
+  });
+}
 
